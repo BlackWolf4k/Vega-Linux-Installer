d996125d650d55a83af5ac233e7ad1c43767cec5
BlackWolf4k <64918128+BlackWolf4k@users.noreply.github.com>
2021-04-21 17:23:00 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -88,9 +88,9 @@ touch /etc/hostname
 echo -e "$hostname" > /etc/hostname
 echo -e "\n127.0.0.1    localhost\n::1      localhost\n127.0.1.1    $hostname.localdomain   $hostname" >> /etc/hosts
 echo -e "Please insert root password:"
-echo "${root_password}\n${root_password}" | passwd
+echo -e "${root_password}\n${root_password}" | passwd
 useradd -m "$username"
-echo "${user_password}\n${user_password}" | passwd "${username}"
+echo -e "${user_password}\n${user_password}" | passwd "${username}"
 usermod -aG wheel,audio,video,optical,storage "$username"
 pacman -S sudo --noconfirm
 sed -i 's/#%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' visudo
