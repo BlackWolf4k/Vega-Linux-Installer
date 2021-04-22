5eb5f45da56c783df4326feaba10520b5b9ec343
BlackWolf4k <64918128+BlackWolf4k@users.noreply.github.com>
2021-04-22 12:02:29 +0200

    Add files via upload

---
+++ pacman-Installer.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+#Change over
+#Utils installer
+
+PACKAGES={
+	#Gnome
+	'xorg'
+	'gnome'
+	'gnome-tweaks'
+	'gnome-nettool'
+	'gnome-usage'
+	'gnome-multi-writer'
+	'xdg-user-dirs-gtk'
+	'chrome-gnome-shell'
+	'adwaita-icon-theme'
+	'nautilus-sendto'
+	'fwupd'
+	'arc-gtk-theme'
+	'seahosrse'
+	'gdm'
+	'xscreensaver'
+	'archlinux-wallpaper'
+	
+
+	#Basese
+	'pacman-contrib'
+	'git'
+	'wget'
+
+	#Service
+	'apache'
+	'php-apache'
+	'mysql'
+
+	#Lenguages
+	'code'
+	'php'
+	'python-pip'
+	'ruby'
+	'arduino'
+	'arduino-cli'
+	'arduino-docs'
+	'arduino-avr-core'
+
+	
+	#Other in Command Line
+	'mlocate'
+	'imagemagick'
+	'nmap'
+	'john'
+	'hydra'
+	'sqlmap'
+	'binwalk'
+	'aircrack-ng'
+	'nikto'
+
+	#Other with GUI
+	'firefox'
+	'vlc'
+	'filezilla'
+	'leafpad'
+	'wireshark-qt'
+	'libreoffice-fresh'
+}
+
+for package in "$"
+do
+	echo "Installing: ${package}"
+	sudo pacman -S "$package" --noconfirm --needed
+done
+
+echo -e "\nFine\n"
